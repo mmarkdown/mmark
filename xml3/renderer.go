6b44e7fd958942655cb53f89a4316caf0642f466
Miek Gieben <miek@miek.nl>
2018-08-02 09:59:07 +0000

    Callout in the renderer
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- xml3/renderer.go
+++ xml3/renderer.go
@@ -1,8 +1,17 @@
 package xml3
 
 // Renderer implements Renderer interface for IETF XMLv3 output.
-type Renderer struct{}
+type Renderer struct {
+	opts RendererOptions
+}
 
 // New creates and configures an Renderer object, which satisfies the Renderer
 // interface.
-func New() *Renderer { return &Renderer{} }
+func New(opts RendererOptions) *Renderer {
+	return &Renderer{opts: opts}
+}
+
+type RendererOptions struct {
+	// Callouts are supported and detected by setting this option to the callout prefix.
+	Callout string
+}
