359cb418d4b740d70bafe6613566b5e99bd70550
Miek Gieben <miek@miek.nl>
2018-08-13 20:31:49 +0200

    fixes
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- mhtml/hook.go
+++ mhtml/hook.go
@@ -17,14 +17,18 @@ func RenderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool
 	case *mast.Reference:
 		reference(w, node, entering)
 		return ast.GoToNext, true
+	case *mast.Title:
+		// nothing yet, here
+		//title(w, node, entering)
+		return ast.GoToNext, true
 	}
 	return ast.GoToNext, false
 }
 
 func references(w io.Writer, node ast.Node, entering bool) {
-	println("references")
+	println("references: TODO")
 }
 
 func reference(w io.Writer, node ast.Node, entering bool) {
-	println("single reference")
+	println("reference: TODO")
 }
