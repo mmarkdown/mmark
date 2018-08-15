969b309a8dfd2cd02441704763628c0aa2dbeaed
Miek Gieben <miek@miek.nl>
2018-08-15 19:52:36 +0200

    Work on the index
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- mhtml/hook.go
+++ mhtml/hook.go
@@ -23,9 +23,15 @@ func RenderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool
 		//title(w, node, entering)
 		return ast.GoToNext, true
 	case *mast.Indices:
+		if !entering {
+			return ast.GoToNext, true
+		}
 		io.WriteString(w, "<h1>Index</h1>\n")
 		return ast.GoToNext, true
 	case *mast.IndexItem:
+		if !entering {
+			return ast.GoToNext, true
+		}
 		indexItem(w, node)
 		return ast.GoToNext, true
 	}
@@ -35,7 +41,7 @@ func RenderHook(w io.Writer, node ast.Node, entering bool) (ast.WalkStatus, bool
 func indexItem(w io.Writer, node *mast.IndexItem) {
 	for i := range node.Items {
 		w.Write(node.Items[i].Item)
-		w.Write([]byte(" "))
+		w.Write([]byte("\n "))
 		w.Write(node.Items[i].Subitem)
 		w.Write([]byte(" "))
 		fmt.Fprintf(w, "%d\n", node.Items[i].ID)
