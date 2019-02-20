6a3b86d7326a209d84138a4aa0a33c18605f7cbb
Miek Gieben <miek@miek.nl>
2019-02-20 07:41:18 +0000

    Add more author elements to the reference struct
    This structure was incomplete; which doesn't matter if you just
    reference RFCs or IDs, but does if you manually add them. Complete the
    struct and add a little test.
    
    Fixes: #61
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- mast/reference/reference.go
+++ mast/reference/reference.go
@@ -5,9 +5,39 @@ import "encoding/xml"
 
 // Author is the reference author.
 type Author struct {
-	Fullname string `xml:"fullname,attr"`
-	Initials string `xml:"initials,attr"`
-	Surname  string `xml:"surname,attr"`
+	Fullname     string        `xml:"fullname,attr,omitempty"`
+	Initials     string        `xml:"initials,attr,omitempty"`
+	Surname      string        `xml:"surname,attr,omitempty"`
+	Role         string        `xml:"role,attr,omitempty"`
+	Organization *Organization `xml:"organization,omitempty"`
+	Address      *Address      `xml:"address,omitempty"`
+}
+
+type Organization struct {
+	Abbrev string `xml:"organization,attr,omitempty"`
+}
+
+// this is copied from ../title.go; it might make sense to unify them, both especially, it we
+// want to allow reference to be given in TOML as well. See #55.
+// Author denotes an RFC author.
+
+// Address denotes the address of an RFC author.
+type Address struct {
+	Phone  string        `xml:"phone,omitempty"`
+	Email  string        `xml:"email,omitempty"`
+	URI    string        `xml:"uri,omitempty"`
+	Postal AddressPostal `xml:"postal,omitempty"`
+}
+
+// AddressPostal denotes the postal address of an RFC author.
+type AddressPostal struct {
+	PostalLine []string `xml:"postalline,omitempty"`
+
+	Streets   []string `xml:"street,omitempty"`
+	Cities    []string `xml:"city,omitempty"`
+	Codes     []string `xml:"code,omitempty"`
+	Countries []string `xml:"country,omitempty"`
+	Regions   []string `xml:"region,omitempty"`
 }
 
 // Date is the reference date.
@@ -20,11 +50,11 @@ type Date struct {
 // Front the reference <front>.
 type Front struct {
 	Title   string   `xml:"title"`
-	Authors []Author `xml:"author"`
+	Authors []Author `xml:"author,omitempty"`
 	Date    Date     `xml:"date"`
 }
 
-// Format is the reference <format>.
+// Format is the reference <format>. This is deprecated in RFC 7991, see Section 3.3.
 type Format struct {
 	Type   string `xml:"type,attr,omitempty"`
 	Target string `xml:"target,attr"`
@@ -36,4 +66,5 @@ type Reference struct {
 	Anchor  string   `xml:"anchor,attr"`
 	Front   Front    `xml:"front"`
 	Format  *Format  `xml:"format,omitempty"`
+	Target  string   `xml:"target,attr"`
 }
