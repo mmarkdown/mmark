a1d8146c27daa30a00f7f3cd98f00020692375e3
Miek Gieben <miek@miek.nl>
2019-02-20 07:49:43 +0000

    Add more author elements to the reference struct (#62)
    This structure was incomplete; which doesn't matter if you just
    reference RFCs or IDs, but does if you manually add them. Complete the
    struct and add a little test.
    
    Fixes: #61
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

---
+++ mast/reference/reference_test.go
@@ -0,0 +1,34 @@
+package reference
+
+import (
+	"encoding/xml"
+	"testing"
+)
+
+func TestReferenceOrganization(t *testing.T) {
+	in := []byte(`<reference anchor='IANA' target='https://www.iana.org/assignments/media-types/media-types.xhtml'>
+    <front>
+        <title abbrev='IANA'>IANA Media Types</title>
+        <author>
+            <organization>IANA</organization>
+        </author>
+        <date month='February' year='2019'/>
+    </front>
+</reference>
+`)
+	expect := `<reference anchor="IANA" target="https://www.iana.org/assignments/media-types/media-types.xhtml"><front><title>IANA Media Types</title><author><organization></organization></author><date year="2019" month="February"></date></front></reference>`
+
+	var x Reference
+	if err := xml.Unmarshal(in, &x); err != nil {
+		t.Errorf("failed to unmarshal reference: %s: %s", in, err)
+	}
+	out, err := xml.Marshal(x)
+	if err != nil {
+		t.Errorf("failed to marshal reference: %s", err)
+	}
+	str := string(out)
+
+	if str != expect {
+		t.Errorf("expected\n%s\ngot\n%s", expect, str)
+	}
+}
