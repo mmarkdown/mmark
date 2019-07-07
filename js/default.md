fc4a52c311c057ba5e7dcd4d8ea208d8aae08476
Richard Barnes <rlb@ipv.sx>
2019-07-07 17:21:04 -0400

    Add an initial web interface, ported from draftr-js

---
+++ js/default.md
@@ -0,0 +1,36 @@
+%%%
+title = "Example Protocol Specification"
+abbrev = "EPS"
+obsoletes = []
+ipr= "trust200902"
+area = "Internet"
+workgroup = "Example Working Group"
+submissiontype = "IETF"
+keyword = [""]
+
+[seriesInfo]
+name = "RFC"
+value = "8341"
+stream = "IETF"
+status = "standard"
+
+[[author]]
+initials="J.Q."
+surname="Public"
+fullname="John Q. Public"
+organization = "Yoyodyne"
+  [author.address]
+  email = "jqs@yoyodyne.example.com"
+%%%
+
+.# Abstract
+
+Abstract goes here
+
+{mainmatter}
+
+# Introduction
+
+Introduction goes here, followed by more text and more sections,
+etc.
+
--- js/main.go
+++ js/main.go
@@ -13,78 +13,9 @@ import (
 
 var standardComments = [][]byte{[]byte("//"), []byte("#")}
 
-var sampleData = `
-%%%
-title = "Network Configuration Access Control Model"
-abbrev = "NACM"
-obsoletes = [6536]
-ipr= "trust200902"
-area = "Internet"
-workgroup = "Network Working Group"
-submissiontype = "IETF"
-keyword = [""]
-date = 2018-03-01T00:00:00Z
-
-[seriesInfo]
-name = "RFC"
-value = "8341"
-stream = "IETF"
-status = "standard"
-
-[[author]]
-initials="A."
-surname="Bierman"
-fullname="Andy Bierman"
-abbrev = "YumaWorks"
-organization = "YumaWorks"
-  [author.address]
-  email = "andy@yumaworks.com"
-  [author.address.postal]
-  city = "Simi Valley"
-  street = "685 Cochran St."
-  code = "CA 93065"
-  postalline= ["Suite #160"]
-  country = "United States of America"
-[[author]]
-initials="M."
-surname="Bjorklund"
-fullname="Martin Bjorklund"
-abbrev = "Tail-f Systems"
-organization = "Tail-f Systems"
-  [author.address]
-  email = "mbj@tail-f.com"
-%%%
-
-.# Abstract
-
-The standardization of network configuration interfaces for use with the Network Configuration
-Protocol (NETCONF) or the RESTCONF protocol requires a structured and secure operating environment
-that promotes human usability and multi-vendor interoperability.  There is a need for standard
-mechanisms to restrict NETCONF or RESTCONF protocol access for particular users to a preconfigured
-subset of all available NETCONF or RESTCONF protocol operations and content.  This document defines
-such an access control model.
-
-This document obsoletes RFC 6536.
-
-{mainmatter}
-
-# Introduction
-
-The Network Configuration Protocol (NETCONF) and the RESTCONF protocol do not provide any standard
-mechanisms to restrict the protocol operations and content that each user is authorized to access.
-
-There is a need for interoperable management of the controlled access to administrator-selected
-portions of the available NETCONF or RESTCONF content within a particular server.
-
-This document addresses access control mechanisms for the Operations and Content layers of NETCONF,
-as defined in [@!RFC6241]; and RESTCONF, as defined in [@!RFC8040].  It contains three main
-sections:
-`
-
 func main() {
 	js.Global.Set("mmark", map[string]interface{}{
 		"NewDocument": NewDocument,
-		"SampleData":  sampleData,
 	})
 }
 
@@ -118,11 +49,11 @@ func NewDocument(data string) *js.Object {
 	return js.MakeWrapper(doc)
 }
 
-func (doc *Document) HTMLFragment() string {
+func (doc *Document) ToHTML() string {
 	opts := html.RendererOptions{
 		Comments:       [][]byte{[]byte("//"), []byte("#")},
 		RenderNodeHook: mhtml.RenderHook,
-		Flags:          html.CommonFlags | html.FootnoteNoHRTag | html.FootnoteReturnLinks,
+		Flags:          html.CommonFlags | html.FootnoteNoHRTag | html.FootnoteReturnLinks | html.CompletePage,
 		Title:          doc.title,
 	}
 
