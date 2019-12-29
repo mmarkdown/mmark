9d9996b75cf4ef84520c3cd336e37bf86bf7392f
Miek Gieben <miek@miek.nl>
2019-12-29 22:30:31 +0100

    Add RFC 8700
    Put rfc 8700 in the mmarkified rfc. This needed a small change in the
    comment handling because I didn't add the full RFC at once. Other than
    that change - it flawlessly imported.
    
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- render/xml/comment.go
+++ render/xml/comment.go
@@ -2,12 +2,12 @@ package xml
 
 import "bytes"
 
-// Iscomment detects if a html span is a comment: <!--SPACE .... SPACE--!>. Return the comment text and true is so.
+// Iscomment detects if a html span is a comment: <!-- .... --!>. Return the comment text and true is so.
 func IsComment(data []byte) ([]byte, bool) {
-	if !bytes.HasPrefix(data, []byte("<!-- ")) {
+	if !bytes.HasPrefix(data, []byte("<!--")) {
 		return nil, false
 	}
-	if !bytes.HasSuffix(data, []byte(" -->")) {
+	if !bytes.HasSuffix(data, []byte("-->")) {
 		return nil, false
 	}
 
